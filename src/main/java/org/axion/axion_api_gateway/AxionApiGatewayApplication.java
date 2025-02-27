af5ac89f8037f995fb620aed47dbe0703a6c5eee
Tomasz Nowak <nowaktomasz2@gmail.com>
2025-02-27 10:14:48 +0100

    DG-61 Open new port for Vite UI.

--- src/main/java/org/axion/axion_api_gateway/AxionApiGatewayApplication.java
+++ src/main/java/org/axion/axion_api_gateway/AxionApiGatewayApplication.java
@@ -24,7 +24,7 @@ public class AxionApiGatewayApplication {
                                         rule -> {
                                             rule.allowHost(
                                                     "https://app.disc-golf.pl",
-                                                    "http://localhost:3000"
+                                                    "http://localhost:5173"
                                             );
                                             rule.allowCredentials = true;
                                         }
